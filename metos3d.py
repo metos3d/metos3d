629a944592954b969baa3065f9ebcec907bbb017
jpicau <jpi@informatik.uni-kiel.de>
2013-04-17 15:31:10 +0200

    started to implement main script

--- metos3d.py
+++ metos3d.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python
 
-print "Hello, I'm Metos3D!"
-print "Hello!"
+#
+#   main
+#
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print '''Example usage:
+  ./metos3d [info | home | options ] [FORMULA...]
+  brew install FORMULA...
+  brew uninstall FORMULA...
+  brew search [foo]
+  brew list [FORMULA...]
+  brew update
+  brew upgrade [FORMULA...]
 
+Troubleshooting:
+  brew doctor
+  brew install -vd FORMULA
+  brew [--env | --config]
+
+Brewing:
+  brew create [URL [--no-fetch]]
+  brew edit [FORMULA...]
+  open https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+
+Further help:
+  man brew
+  brew home'''
+    else:
+        sys.exit(0)
