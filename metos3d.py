a303cc84179a05abcf54786969c92059ac03b2f8
jpicau <jpi@informatik.uni-kiel.de>
2013-04-18 13:39:09 +0200

    implemented updates for parts

--- metos3d.py
+++ metos3d.py
@@ -35,18 +35,81 @@ def execute_command(cmd, msg):
 #   do_selfupdate
 #
 def do_selfupdate():
-    print "# Updating Metos3D ..."
+    print "# Selfupdate ..."
     # update metos3d
     cmd = "cd " + mdir + "/metos3d/; git pull; cd ../../"
-    msg = "# Updated metos3d script"
-    errmsg = "Could not update metos3d script."
+    msg = "# Selfupdate successfully applied."
+    errmsg = "Could not update myself."
     if not execute_command(cmd, msg) == 0: sys.exit(0)
 
 #
-#   do_data
+#   do_update_data
 #
-def do_data():
-    print "# data ..."
+def do_update_data():
+    print "# Updating data ..."
+    # update data
+    cmd = "cd " + mdir + "/data/; git pull; cd ../../"
+    msg = "# Data update successfully applied."
+    errmsg = "Could not update data."
+    if not execute_command(cmd, msg) == 0: sys.exit(0)
+
+#
+#   do_update_model
+#
+def do_update_model():
+    print "# Updating model ..."
+    # update model
+    cmd = "cd " + mdir + "/model/; git pull; cd ../../"
+    msg = "# Model update successfully applied."
+    errmsg = "Could not update model."
+    if not execute_command(cmd, msg) == 0: sys.exit(0)
+
+#
+#   do_update_simpack
+#
+def do_update_simpack():
+    print "# Updating simpack ..."
+    # update simpack
+    cmd = "cd " + mdir + "/simpack/; git pull; cd ../../"
+    msg = "# Simpack update successfully applied."
+    errmsg = "Could not update simpack."
+    if not execute_command(cmd, msg) == 0: sys.exit(0)
+
+#
+#   do_update_all
+#
+def do_update_all():
+    print "# Updating all ..."
+    do_update_data()
+    do_update_model()
+    do_update_simpack()
+
+#
+#   do_update
+#
+def do_update(argv):
+    import sys
+    print "# Update ..."
+    # dispatch subcommand
+    if len(argv) < 3:
+        print "Usage:"
+        print "  ./metos3d update [data | model | simpack | all]"
+        sys.exit(0)
+    status = "unknown"
+    if argv[2] == "data":
+        do_update_data()
+        status = "OK"
+    if argv[2] == "model":
+        do_update_model()
+        status = "OK"
+    if argv[2] == "simpack":
+        do_update_simpack()
+        status = "OK"
+    if argv[2] == "all":
+        do_update_all()
+        status = "OK"
+    if status == "unknown":
+        print "# ERROR: Unknown subcommand:", argv[2]
 
 #
 #   dispatch_subcommand
@@ -56,8 +119,8 @@ def dispatch_subcommand(argv):
     if argv[1] == "selfupdate":
         do_selfupdate()
         status = "OK"
-    if argv[1] == "data":
-        do_data()
+    if argv[1] == "update":
+        do_update(argv)
         status = "OK"
     if status == "unknown":
         print "# ERROR: Unknown command:", argv[1]
@@ -70,7 +133,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print "Example usage:"
         print "  ./metos3d selfupdate"
-        print "  ./metos3d data"
+        print "  ./metos3d update [data | model | simpack | all]"
 #        print "  ./metos3d model"
 #        print "  ./metos3d simpack"
 #        print "  ./metos3d petsc"
