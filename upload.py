3166d5a93960f71c367fca1b1c692de4bc60f5be
jpicau <jpi@informatik.uni-kiel.de>
2018-10-01 15:05:04 +0200

    1.0.4

--- upload.py
+++ upload.py
@@ -36,3 +36,17 @@ if __name__ == "__main__":
     version = sys.argv[1]
     print("Preparing version ....... " + version)
 
+
+#    filepath_in = "metos3d/__init__.template.py"
+#    filepath_out = "metos3d/__init__.py"
+#
+#f_in = open(filepath_in, "r", encoding="utf-8")
+#f_out = open(filepath_out, "w", encoding="utf-8")
+#
+#print("Reading from ............ " + filepath_in)
+#print("Writing to .............. " + filepath_out)
+#
+#f_out.write(f_in.read().format(version=version))
+#
+#f_in.close()
+#f_out.close()
