13b910f22d5ae82c18ef4aaa2e6db2f80ee67c8b
jpicau <jpi@informatik.uni-kiel.de>
2018-10-01 16:13:18 +0200

    1.0.4

--- upload.py
+++ upload.py
@@ -36,17 +36,35 @@ if __name__ == "__main__":
     version = sys.argv[1]
     print("Preparing version ....... " + version)
 
+    version_file = "metos3dpy/version.py"
+    print("Writing to .............. " + version_file)
+    f = open(version_file, "w", encoding="utf-8")
+    f.write("__version__ = \"{0}\"\n".format(version))
+    f.close()
 
-#    filepath_in = "metos3d/__init__.template.py"
-#    filepath_out = "metos3d/__init__.py"
-#
-#f_in = open(filepath_in, "r", encoding="utf-8")
-#f_out = open(filepath_out, "w", encoding="utf-8")
-#
-#print("Reading from ............ " + filepath_in)
-#print("Writing to .............. " + filepath_out)
-#
-#f_out.write(f_in.read().format(version=version))
-#
-#f_in.close()
-#f_out.close()
+    cmd = "git ci -am '{0}'".format(version)
+    print("Commiting ............... " + cmd)
+    os.system(cmd)
+
+    cmd = "git tag -a -m '{0}' {0}".format(version)
+    print("Tagging ................. " + cmd)
+    os.system(cmd)
+
+
+#git push --set-upstream origin refs/heads/1.0.4:refs/heads/1.0.4
+#git push --follow-tags --set-upstream origin refs/heads/1.0.4:refs/heads/1.0.4
+#cmd = "git push --follow-tags"
+#    print("Pushing ................. " + cmd)
+#    os.system(cmd)
+
+#    cmd = "python setup.py sdist"
+#    print("Creating distribution ... " + cmd)
+#    os.system(cmd)
+
+#cmd = "twine upload dist/metos3d-{0}.tar.gz".format(version)
+#print("Uploading to PyPI ....... " + cmd)
+#os.system(cmd)
+
+
+
+#conda config --set anaconda_upload yes
